d9cb2347b80c8f355659b32262ac1592bbbb3dbb
sabahat <sabahat.hussain@dcmail.ca>
2021-12-07 13:28:33 -0500

    Changed child theme name

--- themes/spatime-child/functions.php
+++ themes/spatime-child/functions.php
@@ -1,10 +1,37 @@
 <?php
+/**
+ * Functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
 
-add_action( 'wp_enqueue_scripts', 'yourown_enqueue_styles' );
-    function yourown_enqueue_styles() {
-        wp_enqueue_style( 'parent-style', get_stylesheet_directory_uri().'/style.css' );
-        wp_enqueue_style( 'child-style', get_stylesheet_directory_uri().'/css/style.css' );
 
+
+add_action( 'wp_enqueue_scripts', 'my_plugin_add_stylesheet' );
+function my_plugin_add_stylesheet() {
+    wp_enqueue_style( 'my-style', get_stylesheet_directory_uri() . '/style.css', false, '1.0', 'all' );
 }
 
-?>
+
+
+function spaTimeTheme_enqueue_scripts() {
+    $parenthandle = 'twentytwentyone-style'; // This is 'twentytewntyone-style' for the perfect theme.
+    $theme = wp_get_theme();
+    wp_enqueue_style( 
+        $parenthandle, get_template_directory_uri() . '/style.css', 
+        array(),  // if the parent theme code has a dependency, copy it to here
+        $theme->parent()->get('Version')
+    );
+    wp_enqueue_style( 
+        'perfect-style', 
+        get_stylesheet_uri(),
+        array( $parenthandle ),
+        $theme->get('Version') // this only works if you have Version in the style header
+    );
+}
+
+add_action( 'wp_enqueue_scripts', 'spaTimeTheme_enqueue_scripts' );
